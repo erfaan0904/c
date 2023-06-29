20dd51909e68e02ee79fe6f1ef68854b65cccd10
Erfan najafi <87322931+erfaan0904@users.noreply.github.com>
2023-06-29 09:04:47 +0330

    Add files via upload

---
+++ Addition and subtraction of arrays.cpp
@@ -0,0 +1,72 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[2][2], b[2][2], add[2][2] = { 0 }, sub[2][2] = { 0 };
+	//--------------------Erntering arrays--------------------------
+	cout << "--------------------------------" << endl;
+	cout << "enter first array : " << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cout << " a[" << i + 1 << "][" << j + 1 << "] : ";
+			cin >> a[i][j];
+		}
+	}
+	cout << "--------------------------------" << endl;
+	cout << "enter second array : " << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cout << " b[" << i + 1 << "][" << j + 1 << "] : ";
+			cin >> b[i][j];
+		}
+	}
+	//-------------Show arrays befor multiplication-----------------
+	cout << "--------------------------------" << endl;
+	cout << "first array : " << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+			cout << a[i][j] << " ";
+		cout << endl;
+	}
+	cout << "second array : " << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+			cout << b[i][j] << " ";
+		cout << endl;
+	}
+	//--------------------Addition arrays---------------------------
+	cout << "--------------------------------" << endl;
+	cout << "first aart + second array : " << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+
+				add[i][j] = a[i][j] + b[i][j];
+				cout << add[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << "--------------------------------" << endl;
+	//------------------subtraction arrays--------------------------
+	cout << "first aart + second array : " << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+
+			add[i][j] = a[i][j] - b[i][j];
+			cout << add[i][j] << " ";
+		}
+		cout << endl;
+	}
+	//--------------------------------------------------------------
+	return 0;
+}
